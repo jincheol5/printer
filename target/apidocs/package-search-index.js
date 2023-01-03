1058fa18fe8cdba80c987f9c6583620170133a54
jincheol <oscar4293@gmail.com>
2023-01-03 13:53:58 +0900

    first upload

---
+++ target/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.jincheol5.printer"}];updateSearchResults();
---
+++ target/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"io.github.jincheol5.printer","l":"App"},{"p":"io.github.jincheol5.printer","l":"Printer"}];updateSearchResults();
